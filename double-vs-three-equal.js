8378bdfd087092e8fd10e9e55dedc0a5ef56a69e
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-02-02 10:24:34 +0600

    double three equal

---
+++ double-vs-three-equal.js
@@ -0,0 +1,22 @@
+const first = 4;
+const second = "4";
+
+const third = 1;
+const fourth = true;
+
+const fifth = 0;
+const sixth = false;
+
+if(third == fourth){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
+
+if(third === fourth){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
