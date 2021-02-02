26c6a66ed935fadd148f3179410008b9d57a8de2
Md. Rasel Hossain <rasel.as.cse@gmail.com>
2021-02-02 08:44:36 +0600

    check true false

---
+++ trueFalse.js
@@ -0,0 +1,13 @@
+// truthy = '0', " ", [], {}, true, 'false'
+
+
+// falsy = 0, "", undefined, null, NaN, false
+
+
+const age = 4;
+if(0){
+    console.log('condition is true');
+}
+else{
+    console.log('condition is false');
+}
